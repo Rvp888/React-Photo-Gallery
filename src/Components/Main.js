28c46cb9827b96b25b2d4616e8560c1d3d40406d
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-05 21:31:36 +0530

    modify files

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,15 +8,16 @@ export default function Main() {
     const { images } = useContext(appContext);
     const [ listView, setListView ] = useState(false);
     
-  return (
-    <div className='main'>
-      <div className={`main_gallery ${listView ? "list-view" : "grid-view"}`} >
-        {images.map((image) => (
-            <div key={image.id} className='img-div' >
-                <img src={image.imgUrl} />
+    return (
+        <div className='main'>
+            <button className='change-view-btn' onClick={() => setListView(!listView)} >Change View</button>
+            <div className={`main_gallery ${listView ? "list-view" : "grid-view"}`} >
+                {images.map((image) => (
+                    <div key={image.id} className='img-div' >
+                        <img src={image.imgUrl} />
+                    </div>
+                ))}
             </div>
-        ))}
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
