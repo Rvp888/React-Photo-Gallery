ce788a6a07e4619b3f94a15b0705b4f8dccd34d4
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-05 21:45:36 +0530

    modify files

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -1,15 +1,28 @@
 
 import React from 'react';
 
-function handleSubmit() {
-    
-}
+
 
 export default function Upload() {
+
+    const [ photoUrl, setPhotoUrl ] = useState('');
+    const [ photoName, setPhotoName ] = useState('');
+
+    function handleSubmit() {
+
+    }
+
+
     return (
         <div className='upload-box'>
+            <h2>Uplod Photo</h2>
             <form className='upload-form' onSubmit={handleSubmit}>
-
+                <label>Photo URL</label>
+                <input type='text' onChange={(e) => } />
+                <br/>
+                <label>Photo Name</label>
+                <input type='text' onChange={(e) => } />
+                <br/>
             </form>
         </div>
     )
