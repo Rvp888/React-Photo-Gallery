e0905fbc51b630600630223dd81123e994c5d81b
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-05 19:22:59 +0530

    modify files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 export default function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='header'>
+            <h2 className='app_title' >Photo-Gallery</h2>
+            <input type='search' placeholder='Search...' />
+            <button>+</button>
+        </div>
+    )
 }
