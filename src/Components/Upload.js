313394ff6b7cffb8c5512a53c0ee40639909e56c
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-05 21:47:28 +0530

    modify files

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -18,11 +18,13 @@ export default function Upload() {
             <h2>Uplod Photo</h2>
             <form className='upload-form' onSubmit={handleSubmit}>
                 <label>Photo URL</label>
-                <input type='text' onChange={(e) => } />
+                <input type='text' onChange={(e) => setPhotoUrl(e.target.value)} />
                 <br/>
                 <label>Photo Name</label>
-                <input type='text' onChange={(e) => } />
+                <input type='text' onChange={(e) => setPhotoName(e.target.value)} />
                 <br/>
+                <button className='upload-btn'>Upload</button>
+                <button className='cancel-btn'>Cancel</button>
             </form>
         </div>
     )
