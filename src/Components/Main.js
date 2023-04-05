cddbacebdb9606f86737e2b09ab766ae8bd70516
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-05 20:57:14 +0530

    modify files

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 import { useContext } from 'react';
 import { appContext } from '../App';
@@ -6,10 +6,11 @@ import { appContext } from '../App';
 export default function Main() {
 
     const { images } = useContext(appContext);
+    const [ listView, setListView ] = useState(false);
     
   return (
     <div className='main'>
-      <div className='main_gallery' >
+      <div className={`main_gallery ${listView ? "list-view" : "grid-view"}`} >
         {images.map((image) => (
             <div key={image.id} className='img-div' >
                 <img src={image.imgUrl} />
